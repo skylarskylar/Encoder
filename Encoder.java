5665eadd40b77d26f4bd6575e285b272b3767570
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-11 12:51:18 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -6,33 +6,12 @@ public class Encoder {
 	ArrayList<Character> file = new ArrayList<Character>();
 
 	//dont talk about it 
-	public Encoder(){
-		arr.add("a");
-		arr.add("b");
-		arr.add("c");
-		arr.add("d");
-		arr.add("e");
-		arr.add("f");
-		arr.add("g");
-		arr.add("h");
-		arr.add("i");
-		arr.add("j");
-		arr.add("k");
-		arr.add("l");
-		arr.add("m");
-		arr.add("n");
-		arr.add("o");
-		arr.add("p");
-		arr.add("q");
-		arr.add("r");
-		arr.add("s");
-		arr.add("t");
-		arr.add("u");
-		arr.add("v");
-		arr.add("w");
-		arr.add("x");
-		arr.add("y");
-		arr.add("z");
+	public Encoder()
+	{
+		for (int i=0; i<95; i++)
+		{
+			arr.add(""+(char)(i+32)); //inputs values into table
+		}
 	}
 
 	public void encodeText(String fileName) throws IOException{
